3827372cb6e207324aaadbc31a673aff6939d207
pablousegreen25 <pablo.gonzalez@openpay.mx>
2019-10-29 12:11:11 -0600

    003

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const verify = require("./verifyToken");
 const User = require("../model/User");
+const {registerValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
 
 router.get("/", verify, async (req, res) => {
   const us = await User.findOne({ _id: req.user._id });
@@ -14,5 +16,39 @@ router.get("/", verify, async (req, res) => {
     }
   });
 });
+
+//register
+router.post("/register", verify, async (req, res) => {
+    //Lets validate the data before we a user
+    console.log('Body is: ', req.body);
+    console.log('Name is: ', req.body.name);
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+  
+    //check the user exists
+    const emailExists = await User.findOne({email: req.body.email});
+    if(emailExists){
+      return res.status(401).send('Email already exits');
+    }
+  
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+  
+    //create new User
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword
+    });
+  
+    try{
+      //save the new User
+        const savedUser = await user.save();
+        res.status(200).send({user: user._id});
+    }catch(err){
+        res.status(400).send(err);
+    }
+  });
 
 module.exports = router;
